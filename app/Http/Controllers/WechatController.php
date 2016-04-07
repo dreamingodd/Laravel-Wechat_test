50ea9ce02c51f1eb39dfebdf94c45a5aff38e1f2
System Administrator <root@ye-wendadeair.lan>
2016-04-07 15:06:24 +0800

    initialazation

---
+++ app/Http/Controllers/WechatController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class WechatController extends Controller
+{
+    /**
+    * 处理微信的请求消息
+    * authentication and user post data recipient.
+    * @return string
+    */
+   public function serve()
+   {
+    //    Log::info('request arrived.');  注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
+
+       $wechat = app('wechat');
+       $wechat->server->setMessageHandler(function($message){
+           return "This is Casarover Laravel Wechat Test";
+       });
+
+    //    Log::info('return response.');
+
+       return $wechat->server->serve();
+   }
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['middleware' => ['web']], function(){
+    // Don't put url here.
+});
+
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/wechat/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+    });
+});
+
+Route::any('/wechat', "WechatController@serve");
